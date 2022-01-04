f558d120702fa4de3ddd5b95b1214af5fe2be3cd
mancioks <mantas.kryzevicius@gmail.com>
2022-01-04 21:11:49 +0200

    username function added

--- functions.php
+++ functions.php
@@ -1,1 +1,31 @@
 <?php
+
+$userEmail = "belekas@beleka.lt";
+
+if(isEmailValid($userEmail)){
+    echo clearEmail($userEmail);
+}else{
+    echo 'Emailas nevalidus';
+}
+
+function isEmailValid($email){
+    if (str_contains($email, '@') && str_contains($email, '.')) {
+        return true;
+    }
+    return false;
+}
+
+
+function clearEmail($email){
+    $emailLowercases = strtolower($email);
+    return trim($emailLowercases);
+}
+
+$name = "Mantas";
+$surname = "Kryževičius";
+
+function getNickName($name, $surName) {
+    return strtolower(substr($name, 0, 3).substr($surName, 0, 3));
+}
+
+echo getNickName($name, $surname);
