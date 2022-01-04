2212492f4f09dc93ae8b9a60012e4941b9094af1
mancioks <mantas.kryzevicius@gmail.com>
2022-01-04 22:31:06 +0200

    functions include to index.php

--- functions.php
+++ functions.php
@@ -1,39 +1,44 @@
 <?php
+//
+//$userEmail = "belekas@beleka.lt";
+//
+//if(isEmailValid($userEmail)){
+//    echo clearEmail($userEmail);
+//}else{
+//    echo 'Emailas nevalidus';
+//}
+//
+//function isEmailValid($email){
+//    if (str_contains($email, '@') && str_contains($email, '.')) {
+//        return true;
+//    }
+//    return false;
+//}
+//
+//
 
-$userEmail = "belekas@beleka.lt";
-
-if(isEmailValid($userEmail)){
-    echo clearEmail($userEmail);
-}else{
-    echo 'Emailas nevalidus';
-}
-
-function isEmailValid($email){
-    if (str_contains($email, '@') && str_contains($email, '.')) {
-        return true;
-    }
-    return false;
-}
-
-
+/**
+ * @param $email
+ * @return string
+ */
 function clearEmail($email){
     $emailLowercases = strtolower($email);
     return trim($emailLowercases);
 }
+//
+//$name = "Mantas";
+//$surname = "Kryževičius";
+//
+//function getNickName($name, $surName) {
+//    return strtolower(substr($name, 0, 3).substr($surName, 0, 3)).mt_rand(1, 100);
+//}
+//
+//echo getNickName($name, $surname);
+//
 
-$name = "Mantas";
-$surname = "Kryževičius";
 
-function getNickName($name, $surName) {
-    return strtolower(substr($name, 0, 3).substr($surName, 0, 3)).mt_rand(1, 100);
-}
-
-echo getNickName($name, $surname);
-
-
-
-$title = "Musu kazkokia tai antraste";
-echo getSlug($title);
+//$title = "Musu kazkokia tai antraste";
+//echo getSlug($title);
 /**
  * @param $title //generate slug from title
  * @return string //return slug
--- index.php
+++ index.php
@@ -1,100 +1,111 @@
 <?php
-$products = [
-    [
-        'name' => 'Siulai',
-        'price' => 12.4,
-        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
-    ],
-    [
-        'name' => 'adata',
-        'price' => 1.99,
-        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
-    ],
-    [
-        'name' => 'virbalai',
-        'price' => 3.99,
-        'special_price' => 2.49,
-        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
-    ],
-    [
-        'name' => 'Siulai',
-        'price' => 12.4,
-        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
-    ],
-    [
-        'name' => 'adata',
-        'price' => 1.99,
-        'special_price' => 0.99,
-        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
-    ],
-    [
-        'name' => 'virbalai',
-        'price' => 3.99,
-        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
-    ],
-    [
-        'name' => 'Siulai',
-        'price' => 12.4,
-        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
-    ],
-    [
-        'name' => 'adata',
-        'price' => 1.99,
-        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
-    ],
-    [
-        'name' => 'virbalai',
-        'price' => 3.99,
-        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
-    ],
-    [
-        'name' => 'Siulai',
-        'price' => 12.4,
-        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
-    ],
-    [
-        'name' => 'adata',
-        'price' => 1.99,
-        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
-    ],
-    [
-        'name' => 'virbalai',
-        'price' => 3.99,
-        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
-    ],
-    [
-        'name' => 'Siulai',
-        'price' => 12.4,
-        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
-    ],
-    [
-        'name' => 'adata TEST',
-        'price' => 1.99,
-        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
-    ],
-    [
-        'name' => 'virbalai',
-        'price' => 3.99,
-        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
-    ],
-];
+//$products = [
+//    [
+//        'name' => 'Siulai',
+//        'price' => 12.4,
+//        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+//    ],
+//    [
+//        'name' => 'adata',
+//        'price' => 1.99,
+//        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
+//    ],
+//    [
+//        'name' => 'virbalai',
+//        'price' => 3.99,
+//        'special_price' => 2.49,
+//        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
+//    ],
+//    [
+//        'name' => 'Siulai',
+//        'price' => 12.4,
+//        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+//    ],
+//    [
+//        'name' => 'adata',
+//        'price' => 1.99,
+//        'special_price' => 0.99,
+//        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
+//    ],
+//    [
+//        'name' => 'virbalai',
+//        'price' => 3.99,
+//        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
+//    ],
+//    [
+//        'name' => 'Siulai',
+//        'price' => 12.4,
+//        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+//    ],
+//    [
+//        'name' => 'adata',
+//        'price' => 1.99,
+//        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
+//    ],
+//    [
+//        'name' => 'virbalai',
+//        'price' => 3.99,
+//        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
+//    ],
+//    [
+//        'name' => 'Siulai',
+//        'price' => 12.4,
+//        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+//    ],
+//    [
+//        'name' => 'adata',
+//        'price' => 1.99,
+//        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
+//    ],
+//    [
+//        'name' => 'virbalai',
+//        'price' => 3.99,
+//        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
+//    ],
+//    [
+//        'name' => 'Siulai',
+//        'price' => 12.4,
+//        'img' => 'https://siulupinkles.lt/wp-content/uploads/2021/01/Mezgimo-siulai-ritese-italiski-siulai-kasmyras-kasmyro-siulai-silko-siulai-silkas.jpg'
+//    ],
+//    [
+//        'name' => 'adata TEST',
+//        'price' => 1.99,
+//        'img' => 'https://www.vle.lt/uploads/_CGSmartImage/70839_3-26c56fce05f1ac6e0247f6daa86648aa.jpg'
+//    ],
+//    [
+//        'name' => 'virbalai',
+//        'price' => 3.99,
+//        'img' => 'https://mezgimomanija.lt/wp-content/uploads/2019/02/3397.jpg'
+//    ],
+//];
+//
+//$count = 0;
+//
+//foreach($products as $product){
+//
+//    $count++;
+//
+//    $productPrice = $product["price"];
+//    if(isset($product["special_price"])) {
+//        $productPrice = "<del>".$productPrice."</del> ". $product["special_price"];
+//    }
+//
+//    echo '<img width="60" src="'. $product['img'] .'"/>';
+//    echo '<h2>'. $product["name"] . '</h2>';
+//    echo '<h3>'. $productPrice . '</h3>';
+//    if($count % 3 == 0) {
+//        echo '<hr>';
+//    }
+//
+//}
 
-$count = 0;
+include 'functions.php';
 
-foreach($products as $product){
+$title = "Musu kazkokia tai indexine antraste";
+echo getSlug($title);
 
-    $count++;
+$email = " mantas.kryzevicius@gmail.coM";
 
-    $productPrice = $product["price"];
-    if(isset($product["special_price"])) {
-        $productPrice = "<del>".$productPrice."</del> ". $product["special_price"];
-    }
+echo "<br>";
 
-    echo '<img width="60" src="'. $product['img'] .'"/>';
-    echo '<h2>'. $product["name"] . '</h2>';
-    echo '<h3>'. $productPrice . '</h3>';
-    if($count % 3 == 0) {
-        echo '<hr>';
-    }
-
-}
+echo clearEmail($email);
