3d6249e879519ff2ffd4509bec39e88320a1dfca
mancioks <mantas.kryzevicius@gmail.com>
2022-03-07 22:25:00 +0200

    ignore

--- naujas/config.php
+++ naujas/config.php
@@ -5,7 +5,7 @@ define('BASE_URL', 'http://localhost/');
 define('SERVERNAME', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
-define('DB_NAME', 'ads');
+define('DB_NAME', 'autoplius');
 
 define('PROJECT_ROOT_DIR', __DIR__);
 define('DEBUG_MODE', true);
